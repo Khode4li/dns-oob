561cee6c0a51ba5343d8694c0b44f221a43d2495
Khode4li <88293279+Khode4li@users.noreply.github.com>
2024-03-08 20:53:46 +0330

    Fix  send nothing

--- app/index.php
+++ app/index.php
@@ -3,6 +3,8 @@ require __DIR__.'/vendor/autoload.php';
 $cache = new Cache\Cache();
 if ($cache->get('q') === null) {
     $q = file_get_contents('php://stdin');
+    if ($q === '')
+        exit();
     $cache->set('q', $q, 10);
     \Config\registry::set('q', $q);
     require_once 'config/conf.php';
