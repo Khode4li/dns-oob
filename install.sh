64a91e73abdeccd282d8a95a29714a652cbbe94a
Khode4li <88293279+Khode4li@users.noreply.github.com>
2024-09-21 17:18:46 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,9 +20,9 @@ elif [ "$notif" = "both" ]; then
         sed -i "s#registry::set('TG_CHAT_ID', '');#registry::set('TG_CHAT_ID', '$chatid');#g" ./app/config/conf.php
 fi
 
-read -p "enter the subdomain name that you want to use for DNS OOB(for example: `oob`.domain.com): " subd
+read -p "enter the subdomain name that you want to use for DNS OOB(for example: (oob).domain.com): " subd
 sed -i "s/sub-d/$subd/g" Dockerfile
-read -p "enter your domain name (oob.`domain.com`): " myd
+read -p "enter your domain name (oob.(domain.com)): " myd
 mydes=$(echo $myd | sed 's/\./\\\./g')
 sed -i "s/my-d-es/$mydes/g" Dockerfile
 sed -i "s/my-dd/$myd/g" Dockerfile
